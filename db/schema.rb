a0df36c2d266ca72a8f7c28e1c19ad5db589f216
indysk <87873292+indysk@users.noreply.github.com>
2021-08-19 12:51:22 +0900

    activestorageのmigrateを編集してrecord_idがstringとなるよう編集し，activestorageを正常に動作させることに成功

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_08_18_141729) do
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
+    t.string "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
