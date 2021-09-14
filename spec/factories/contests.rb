99ce726e3b444e1720bb6cb73c074bc9f073c0ce
indysk <87873292+indysk@users.noreply.github.com>
2021-09-14 17:11:49 +0900

    [add]Rspecの調整

--- spec/factories/contests.rb
+++ spec/factories/contests.rb
@@ -2,7 +2,24 @@ FactoryBot.define do
   factory :contest, class: Contest do
     name { "User_Contest" }
     association :user, factory: :user
+    association :user, factory: :user
   end
+  contest_Tom_0 = Contest.create!(
+    name: 'コンテスト_Tom_募集前',
+    user_id: user_Tom.id,
+    description: 'コンテスト_Tom_募集前。の説明',
+    entry_start_at: now.since(10.days),
+    entry_end_at: now.since(10.days),
+    vote_start_at: now.since(10.days),
+    vote_end_at: now.since(10.days),
+    visible_range_entry: 0,
+    visible_range_vote: 0,
+    visible_range_show: 0,
+    visible_range_result: 0,
+    voting_points: 3,
+    num_of_views_in_result: 3,
+    visible_setting_for_user_name: 0
+  )
 
   factory :other_contest, class: Contest do
     name { "OtherUser_Contest" }
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,110 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
+
   before do
-    driven_by(:rack_test)
+    @user = build(:user)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "ユーザ新規登録について" do
+    context "有効なユーザ新規登録のとき" do
+
+    end
+
+    it '新規登録からの登録' do
+      visit signup_path
+
+      fill_in 'Name', with: @user.name
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Confirmation', with: @user.password_confirmation
+      attach_file 'Image', Rails.root.join('spec/fixtures/images/food_pizza.png')
+      expect { click_button 'Create my account' }.to change(User, :count).by(1)
+
+      expect(current_path).to eq user_path(User.find_by(email: @user.email))
+      expect(page.title).to eq "#{@user.name} | Ruby on Rails Tutorial Sample App"
+      expect(page).to have_content @user.name
+      expect(page).to have_selector("img[src$='food_pizza.png']")
+    end
+
+    it '無効なサインアップ → フォームへ戻る' do
+      visit signup_path
+
+      fill_in 'Name', with: ''
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Confirmation', with: @user.password_confirmation
+      attach_file 'Image', Rails.root.join('spec/fixtures/images/food_pizza.png')
+      expect { click_button 'Create my account' }.to change(User, :count).by(0)
+
+      expect(current_path).to eq signup_path
+      expect(find("input#user_name").value).to eq ''
+      expect(find("input#user_email").value).to eq @user.email
+      expect(find("input#user_password").value).to eq nil
+      expect(find("input#user_password_confirmation").value).to eq nil
+      expect(page).to have_content "Name can't be blank"
+    end
+  end
+
+  describe "ログインについて" do
+    context "ユーザが登録されている場合" do
+      before do
+        @user.save
+      end
+
+      it "ログインに失敗 → ログイン画面へリダイレクト" do
+        visit login_path
+
+        fill_in 'Email', with: 'aaaa@example.com'
+        fill_in 'Password', with: @user.password
+        expect { click_button 'Log in' }.to_not change{current_path}
+
+        expect(find("input#session_email").value).to eq nil
+        expect(find("input#session_password").value).to eq nil
+        expect(page).to have_content 'Invalid email/password combination'
+      end
+
+      it "フラッシュメッセージの残留テスト" do
+        #ログイン画面
+        visit login_path
+        fill_in 'Email', with: 'aaaa@example.com'
+        fill_in 'Password', with: @user.password
+        expect { click_button 'Log in' }.to_not change{current_path}
+
+        #ログイン失敗画面
+        expect(find("input#session_email").value).to eq nil
+        expect(find("input#session_password").value).to eq nil
+        expect(page).to have_content 'Invalid email/password combination'
+
+        #ルートパスに遷移
+        visit root_path
+        expect(page).to_not have_content 'Invalid email/password combination'
+      end
+
+      it "ログインからログアウトを通した表示テスト" do
+        visit root_path
+        expect(page).to have_content 'Log in'
+        expect(page).to_not have_content 'Log out'
+        expect(page).to_not have_content 'Profile'
+
+        click_link 'Log in'
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: @user.password
+        expect { click_button 'Log in' }.to change{current_path}.from(login_path).to(user_path(@user))
+        expect(page).to have_content @user.name
+        expect(page).to_not have_content 'Log in'
+        expect(page).to have_content 'Log out'
+        expect(page).to have_content 'Profile'
+
+        click_link 'Log out'
+        expect(page).to have_content 'Log in'
+        expect(page).to_not have_content 'Log out'
+        expect(page).to_not have_content 'Profile'
+      end
+    end
+
+    context "ユーザが登録されていない場合" do
+      ###
+    end
+  end
 end
