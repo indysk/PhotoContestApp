ddd02f6c556b84e2075c0bcaa7a0a1f39e33fa89
indysk <87873292+indysk@users.noreply.github.com>
2021-10-23 22:54:15 +0900

    [add]投票結果の写真にもajax読み込み機能を実装した

--- lib/tasks/recreate_images.rb
+++ lib/tasks/recreate_images.rb
@@ -3,8 +3,12 @@ Photo.all.each do |model|
   begin
     p model.id
     if model.image.present?
+      if model.image.thumb.present?
+        p "#{model.id} already recreated."
+        next
+      end
       model.image.recreate_versions!
-      model.save!
+      p "#{model.id} successed." if model.save!
     end
   rescue
     p "#{model.id} recreate failed."
